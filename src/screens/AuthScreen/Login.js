9deccd6652074a2def5c040d6a8fa95076734e45
unknown <alihaider589@outlook.com>
2020-10-14 09:07:43 -0700

    adding blank project

---
+++ src/screens/AuthScreen/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View,SafeAreaView } from 'react-native'
+
+export default class Login extends Component {
+    render() {
+        return (
+           <SafeAreaView >
+               <View>
+                   
+               </View>
+           </SafeAreaView>
+        )
+    }
+}
